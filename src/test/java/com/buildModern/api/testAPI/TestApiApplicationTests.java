b5b853b65fe213ba7c5349d86360c672af8e1e61
Chandrakant Verma <chandra.verma196@gmail.com>
2023-09-16 20:40:36 +0530

    initial commit

---
+++ src/test/java/com/buildModern/api/testAPI/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildModern.api.testAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
